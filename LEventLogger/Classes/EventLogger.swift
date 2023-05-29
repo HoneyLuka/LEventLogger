16da3eb2049eb295aff3f091562a1c9839635ca7
Luka <rainfyou@gmail.com>
2023-05-29 21:16:04 +0800

    init

---
+++ LEventLogger/Classes/EventLogger.swift
@@ -0,0 +1,29 @@
+//
+//  EventLogger.swift
+//  SleepSentry
+//
+//  Created by Selina on 23/5/2023.
+//
+
+import Foundation
+import Firebase
+
+public extension EventLogger {
+    struct EventType: RawRepresentable, Hashable {
+        public typealias RawValue = String
+        
+        public var rawValue: String
+        
+        public init(rawValue: String) {
+            self.rawValue = rawValue
+        }
+    }
+}
+
+public class EventLogger {
+    private init() {}
+    
+    public class func log(_ type: EventType, parameters: [String: Any]? = nil) {
+        Analytics.logEvent(type.rawValue, parameters: parameters)
+    }
+}
